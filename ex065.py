ff490f2410e179b77875e4c953c0407225eee3b5
Gabriel <gabrielbergamascki@gmail.com>
2025-05-19 17:57:19 -0300

    novos exercícios

--- ex065.py
+++ ex065.py
@@ -16,29 +16,3 @@ while resposta in 'Ss':
 media = soma / quantidade
 print(f'Você digitou {quantidade} números e a média foi {media}')
 print(f'O maior valor foi {maior} e o menor foi {menor}')
-
-
-
-
-
-
-
-
-
-
-'''numero = 0
-maior = 0
-menor = 5000
-soma = 0
-contador = 0
-while numero != 999:
-    numero = int(input('Digite um valor (999 para parar): '))
-    soma += numero
-    contador += 1
-    if numero > maior and numero != 999:
-        maior = numero
-    elif numero < menor:
-        menor = numero
-media = (soma - 999) / (contador - 1)
-
-print(f'O maior número digitado foi {maior} e o menor foi {menor}. E a média é de {media}')'''
