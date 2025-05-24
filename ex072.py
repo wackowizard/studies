ecd369fd6642556a88d9f53ba6f6122d03c9a557
Gabriel <gabrielbergamascki@gmail.com>
2025-05-24 14:59:16 -0300

    incício do mundo 3

---
+++ ex072.py
@@ -0,0 +1,12 @@
+numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    escolha = int(input('Digite um número: '))
+    if escolha < 0 or escolha > 20:
+        print('Escolha um número válido entre 0 e 20.')
+    else:
+        print(f'Você digitou {numeros[escolha]}.')
+
+    continuar = input('Quer continuar? [S/N] ').strip().upper()
+    if continuar == 'N':
+        print('Encerrando o programa. Até logo!')
+        break
